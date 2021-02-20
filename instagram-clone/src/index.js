c4a0a1ea4c4566d4b127d41ad599a1d17d621158
Amy <amyjaderichards@googlemail.com>
2021-02-20 15:09:06 +0000

    initial commit

---
+++ instagram-clone/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+    <h1 className="text-red-500">Hello, Amy and Tom</h1>, document.getElementById('root')
+);
